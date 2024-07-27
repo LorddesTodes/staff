255b152cd98689c826956c65f75ac6093ae35dec
LorddesTodes <109075308+LorddesTodes@users.noreply.github.com>
2024-07-28 00:01:09 +0200

    Add files via upload

---
+++ staff/stuff/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using MelonLoader;
+using PMAPI;
+
+[assembly: MelonInfo(typeof(stuff.stuff), "stuff", "1.0.0", "leon")]
+[assembly: MelonGame("PrimitierDev", "Primitier")]
+[assembly: MelonAdditionalDependencies("PMAPI")]
+[assembly: MelonPriority(1)]
+[assembly: PMAPIMod("stuff")]
